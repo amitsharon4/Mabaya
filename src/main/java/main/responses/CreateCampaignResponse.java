a2b96cef4d9423d12a2046aabaf724a291fefd66
Amit Sharon <72415122+amitsharon4@users.noreply.github.com>
2023-01-21 02:02:57 +0200

    Bug fix

--- src/main/java/main/responses/CreateCampaignResponse.java
+++ src/main/java/main/responses/CreateCampaignResponse.java
@@ -3,6 +3,7 @@ package main.responses;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +12,12 @@ import org.springframework.http.ResponseEntity;
 @NoArgsConstructor()
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@EqualsAndHashCode(callSuper = true)
 public class CreateCampaignResponse extends BaseResponse{
-    String campaignId;
-    String name;
-    String startDate;
-    String bid;
+    private String campaignId;
+    private String name;
+    private String startDate;
+    private String bid;
 
     public static ResponseEntity<CreateCampaignResponse> createErrorResponse(String message, HttpStatus status){
         CreateCampaignResponse response = new CreateCampaignResponse();
--- src/main/java/main/responses/ServeAdResponse.java
+++ src/main/java/main/responses/ServeAdResponse.java
@@ -1,11 +1,17 @@
 package main.responses;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor()
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
 public class ServeAdResponse extends BaseResponse{
     private String productId;
 
